7e2524d09cb2decd5ed4288ebacbd01232b17191
Ronald M <ronaldmh20@gmail.com>
2023-10-10 21:40:53 -0400

    addition of reset password functionality with firebase

--- app/src/main/java/com/example/rentals_app/ResetActivity.java
+++ app/src/main/java/com/example/rentals_app/ResetActivity.java
@@ -3,15 +3,25 @@ package com.example.rentals_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ResetActivity extends AppCompatActivity {
 
-    EditText firstName, lastName, email, password, confirmPassword, phoneNumber;
-    Button btnRegister;
+    EditText  email;
+    Button btnReset;
     TextView loginLink, registerLink;
 
     @SuppressLint("MissingInflatedId")
@@ -20,17 +30,37 @@ public class ResetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset);
 
-        firstName = findViewById(R.id.firstName);
-        lastName = findViewById(R.id.lastName);
         email = findViewById(R.id.email);
-        password = findViewById(R.id.userPassword);
-        confirmPassword = findViewById(R.id.confirmPassword);
-        phoneNumber = findViewById(R.id.phoneNumber);
 
-        btnRegister = findViewById(R.id.btnRegister);
+        btnReset = findViewById(R.id.btnReset);
         loginLink = findViewById(R.id.loginLink);
         registerLink = findViewById(R.id.forgotLink);
 
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = email.getText().toString().trim();
 
+                if (userEmail.isEmpty()) {
+                    Toast.makeText(ResetActivity.this, "Enter a valid email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().sendPasswordResetEmail(userEmail)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ResetActivity.this, "A password reset email has been sent", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(ResetActivity.this,LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            } else {
+                                Toast.makeText(ResetActivity.this, "Error sending password reset email", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
+
 }
