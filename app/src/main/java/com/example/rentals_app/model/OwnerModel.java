0eab942194051329761480ff25a85f0d63a966ef
Camilo Pulido <luxor988@gmail.com>
2023-10-19 15:46:13 -0400

    Add apartments parameter for Owners to store its apartments registered

--- app/src/main/java/com/example/rentals_app/model/OwnerModel.java
+++ app/src/main/java/com/example/rentals_app/model/OwnerModel.java
@@ -1,13 +1,23 @@
 package com.example.rentals_app.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class OwnerModel extends UserModel implements Serializable {
 
+    private List<ApartmentModel> apartments;
     public OwnerModel() {
     }
 
     public OwnerModel(String firstName, String lastName, String email, String phone) {
         super(firstName, lastName, email, phone);
+    }
+
+    public List<ApartmentModel> getApartments() {
+        return apartments;
+    }
+
+    public void setApartments(List<ApartmentModel> apartments) {
+        this.apartments = apartments;
     }
 }
