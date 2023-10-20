4b7c662f92a2eead19f0e11fafd434ce58e6c9d4
Ronald M <ronaldmh20@gmail.com>
2023-10-20 11:54:15 -0400

    Feature> ApartmentList fetching db firebase and filling the list view

--- app/src/main/java/com/example/rentals_app/ApartmentsListActivity.java
+++ app/src/main/java/com/example/rentals_app/ApartmentsListActivity.java
@@ -1,43 +1,84 @@
 package com.example.rentals_app;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import com.example.rentals_app.model.UserModel; // Asegúrate de importar UserModel
-
+import android.widget.ListView;
+import com.example.rentals_app.model.UserModel;
+import com.example.rentals_app.model.ApartmentModel;
+import com.example.rentals_app.ApartmentEditActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
 
 public class ApartmentsListActivity extends AppCompatActivity {
 
-    Button createApartmentButton;
     private UserModel loggedUser;
+    private List<ApartmentModel> apartmentsList = new ArrayList<>();
+    private ArrayAdapter<ApartmentModel> apartmentListAdapter;
+    private DatabaseReference databaseReference;
+    Button btnCreate, btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartments_list);
 
-        createApartmentButton = findViewById(R.id.createApartmentButton);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnUpdate = findViewById(R.id.btnUpdate);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ListView apartmentsListView = findViewById(R.id.apartmentsList);
 
-        loggedUser = UserModel.getSession();
-        if (loggedUser != null) {
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("apartments");
 
+        apartmentListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, apartmentsList);
 
-        }
+        apartmentsListView.setAdapter(apartmentListAdapter);
 
-        createApartmentButton.setOnClickListener(new View.OnClickListener() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                apartmentsList.clear();
+                for (DataSnapshot apartmentSnapshot : dataSnapshot.getChildren()) {
+                    ApartmentModel apartment = apartmentSnapshot.getValue(ApartmentModel.class);
+                    if (apartment != null) {
+                        apartmentsList.add(apartment);
+                    }
+                }
+
+                apartmentListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ApartmentsListActivity.this, ApartmentAddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ApartmentsListActivity.this, ApartmentEditActivity.class);
                 startActivity(intent);
             }
         });
@@ -46,7 +87,6 @@ public class ApartmentsListActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-
         return true;
     }
 
