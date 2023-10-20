8e3433ac625f707f76ef954d57dc113b58917584
Ronald M <ronaldmh20@gmail.com>
2023-10-20 09:23:35 -0400

    Feature> Addition of the functionality to add apartments to realtime database

--- app/src/main/java/com/example/rentals_app/ApartmentAddActivity.java
+++ app/src/main/java/com/example/rentals_app/ApartmentAddActivity.java
@@ -7,8 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.rentals_app.model.ApartmentModel;
 import com.example.rentals_app.source.LocationTypes;
 import com.example.rentals_app.source.RentTypes;
@@ -17,55 +18,72 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 public class ApartmentAddActivity extends AppCompatActivity {
 
+    EditText txtTitle, txtPrice, txtUnitNumber, txtAddress, txtLocation, txtPostalCode, txtSize, txtBedrooms, txtBathrooms, txtStageFloor, txtRentType;
+    CheckBox checkHasParking, checkHasHeating;
     Button addButton;
     private DatabaseReference reference;
-    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartment_add);
 
+        txtTitle = findViewById(R.id.txtTitle);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtUnitNumber = findViewById(R.id.txtUnitNumber);
+        txtAddress = findViewById(R.id.txtAddress);
+        txtLocation = findViewById(R.id.txtLocation);
+        txtPostalCode = findViewById(R.id.txtPostalCode);
+        txtSize = findViewById(R.id.txtSize);
+        txtBedrooms = findViewById(R.id.txtBedrooms);
+        txtBathrooms = findViewById(R.id.txtBathrooms);
+        txtStageFloor = findViewById(R.id.txtStageFloor);
+        txtRentType = findViewById(R.id.txtRentType);
+        checkHasParking = findViewById(R.id.checkHasParking);
+        checkHasHeating = findViewById(R.id.checkHasHeating);
         addButton = findViewById(R.id.addButton);
 
-        database = FirebaseDatabase.getInstance();
-        reference = database.getReference("apartments");
-
+        reference = FirebaseDatabase.getInstance().getReference().child("apartments");
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ApartmentModel apartment = new ApartmentModel();
-                apartment.setTitle("2 Bedroom Apartment for Rent in Brampton!");
-                apartment.setPrice(2490.00);
-                apartment.setUnitNumber(1234);
-                apartment.setAddress("Saint-Catherine St W");
-                apartment.setLocation(LocationTypes.CITY_OF_MONTREAL);
-                apartment.setPostalCode("H3H 2T2");
-                apartment.setSize(4);
-                apartment.setBedrooms(3);
-                apartment.setBathrooms(2);
-                apartment.setStageFloor(4);
-                apartment.setHasParking(true);
-                apartment.setHasHeating(true);
-                apartment.setRentType(RentTypes.ROOM_RENTALS_AND_ROOMMATES);
+
+                String title = txtTitle.getText().toString();
+                double price = Double.parseDouble(txtPrice.getText().toString());
+                int unitNumber = Integer.parseInt(txtUnitNumber.getText().toString());
+                String address = txtAddress.getText().toString();
+                String location = txtLocation.getText().toString();
+                String postalCode = txtPostalCode.getText().toString();
+                int size = Integer.parseInt(txtSize.getText().toString());
+                int bedrooms = Integer.parseInt(txtBedrooms.getText().toString());
+                int bathrooms = Integer.parseInt(txtBathrooms.getText().toString());
+                int stageFloor = Integer.parseInt(txtStageFloor.getText().toString());
+                String rentType = txtRentType.getText().toString();
+                boolean hasParking = checkHasParking.isChecked();
+                boolean hasHeating = checkHasHeating.isChecked();
+
+
+                ApartmentModel apartment = new ApartmentModel(unitNumber, address, postalCode, LocationTypes.CITY_OF_MONTREAL, size, bedrooms,
+                        bathrooms, hasParking, hasHeating, stageFloor, RentTypes.ROOM_RENTALS_AND_ROOMMATES, price, title, "", null, null, null, null);
 
                 reference.push().setValue(apartment).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    Class destination = ApartmentsListActivity.class;
                     @Override
                     public void onSuccess(Void unused) {
-
-                        Toast.makeText(ApartmentAddActivity.this, "New apartment success", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ApartmentAddActivity.this, "New apartment added successfully", Toast.LENGTH_SHORT).show();
                     }
+                    Intent intent = new Intent(ApartmentAddActivity.this, destination);
+
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-
-                        Toast.makeText(ApartmentAddActivity.this, "New apartment FAILURE", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ApartmentAddActivity.this, "Failed to add new apartment", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         });
-
     }
 }
