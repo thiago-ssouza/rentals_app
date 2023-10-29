b909e00e30111a3be966a890d356e0f68ddca50a
Camilo Pulido <luxor988@gmail.com>
2023-10-29 13:17:07 -0400

    Add apartment list behaviour

--- app/src/main/java/com/example/rentals_app/ApartmentsListActivity.java
+++ app/src/main/java/com/example/rentals_app/ApartmentsListActivity.java
@@ -10,9 +10,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.rentals_app.model.ApartmentModel;
+import com.example.rentals_app.model.OwnerModel;
+import com.example.rentals_app.model.TenantModel;
 import com.example.rentals_app.model.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +32,8 @@ public class ApartmentsListActivity extends AppCompatActivity {
     private List<ApartmentModel> apartmentsList = new ArrayList<>();
     Button createApartmentButton, updateApartmentButton;
     private UserModel loggedUser;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +46,43 @@ public class ApartmentsListActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("apartments");
+
+
+        Query checkUserDB = reference.orderByChild("title");
+        checkUserDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    UserModel loggedUser;
+                    Class destination;
+                    String dbName = snapshot.child(mAuth.getCurrentUser().getUid()).child("firstName").getValue(String.class);
+
+                    Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, destination);
+
+                    UserModel.setSession(loggedUser);
+                    intent.putExtra("userUUid", mAuth.getCurrentUser().getUid());
+
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "User not found. Please check credentials and try again", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         loggedUser = UserModel.getSession();
         if (loggedUser != null) {
 
-
         }
+
         createApartmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
